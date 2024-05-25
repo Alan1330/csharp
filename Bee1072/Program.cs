8edfa5ffb9e4e62b6dd0182f7aa754fa3fa9c4ac
ALAN TERTULIANO <alan_cfo@outlook.com>
2024-05-25 10:12:37 +0100

    1072 - Intervalo 2

---
+++ Bee1072/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Bee1072 {
+    internal class Program {
+        static void Main(string[] args) {
+            int n = int.Parse(Console.ReadLine());
+
+            int x = n;
+            int dentro = 0;
+            int fora = 0;
+            for (int i = 1; i <= x; i++) {
+                int valor = int.Parse(Console.ReadLine());
+                if (valor >= 10 && valor <= 20) {
+                    dentro = dentro + 1;
+                }
+                else {
+                    fora = fora + 1;
+                }
+            }
+            Console.WriteLine(dentro + " in");
+            Console.WriteLine(fora + " out");
+        }
+    }
+}
