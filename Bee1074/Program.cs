e703bbd16a13f9c8ecf83b537f19793d2f1dc144
ALAN TERTULIANO <alan_cfo@outlook.com>
2024-05-26 18:31:37 +0100

    Bee1074
    bee1074

---
+++ Bee1074/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Bee1074 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            int n = int.Parse(Console.ReadLine());
+
+            int x = n;
+            
+            for(int i = 1; i <= x; i++) {
+                n = int.Parse(Console.ReadLine());
+                if(n == 0) {
+                    Console.WriteLine("NULL");
+                }
+                else if(n % 2 ==0 && n < 0) {
+                    Console.WriteLine("EVEN NEGATIVE");
+                }
+                else if (n % 2 == 0 && n > 0) {
+                    Console.WriteLine("EVEN POSITIVE");
+                }
+                else if (n % 2 != 0 && n < 0) {
+                    Console.WriteLine("ODD NEGATIVE");
+                }
+                else if (n % 2 != 0 && n > 0) {
+                    Console.WriteLine("ODD POSITIVE");
+                }
+            }
+        }
+    }
+}
