f4f4512309c57af8dd7e494cb37d88c49d2718a4
ALAN TERTULIANO <alan_cfo@outlook.com>
2024-05-27 15:09:39 +0100

    curso

---
+++ Bee1096/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Bee1096 {
+    internal class Progrma {
+        static void Main(string[] args) {
+
+            for(int i = 1; i <= 9; i=i+2) {
+                Console.WriteLine("I=" + i + " J=7");
+                Console.WriteLine("I=" + i + " J=6");
+                Console.WriteLine("I=" + i + " J=5");
+            }
+        }
+    }
+}
