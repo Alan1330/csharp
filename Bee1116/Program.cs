9ac7f1ff9da821d475bf75807f056a70efe54b25
ALAN TERTULIANO <alan_cfo@outlook.com>
2024-05-27 16:58:20 +0100

    curso

---
+++ Bee1116/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace Bee116 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <= n; i++) {
+                string[] valores = Console.ReadLine().Split(' ');
+                int val1 = int.Parse(valores[0]);
+                int val2 = int.Parse(valores[1]);
+
+                if(val2 == 0) {
+                    Console.WriteLine("divisao impossivel");
+                }
+                else {
+                    double divisao = (double) val1 / val2; // cast (doble) para forca converter int em double
+                    Console.WriteLine(divisao.ToString("F1", CultureInfo.InvariantCulture));
+                }                
+            }
+        }
+    }
+}
