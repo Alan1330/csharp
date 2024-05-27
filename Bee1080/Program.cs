48f3647a2248e9654b7cd91d255c8ef4b151d084
ALAN TERTULIANO <alan_cfo@outlook.com>
2024-05-27 11:25:12 +0100

    curso

---
+++ Bee1080/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Bee1080 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            int maior = int.Parse(Console.ReadLine());
+            int posicao = 1;
+
+            for (int i = 2; i <= 100; i++) {
+                int n = int.Parse(Console.ReadLine());
+
+                if( n > maior) {
+                    maior = n;
+                    posicao = i;
+                }
+            }
+            Console.WriteLine(maior);
+            Console.WriteLine(posicao);
+        }
+    }
+}
