29abc3f156c21712500a9b5b506b335ce6f577d2
ALAN TERTULIANO <alan_cfo@outlook.com>
2024-06-02 09:53:20 +0100

    curso

---
+++ Bee1142/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Bee1142 {
+    internal class Program {
+        static void Main(string[] args) {
+            int n = int.Parse(Console.ReadLine());
+            int x = 0;
+
+            for (int i = 1; i <= n; i++) {
+                x = x + 1;
+                int a = x;
+                int b = x + 1;
+                int c = x + 2;
+                string texto = "PUM";
+                Console.WriteLine(a + " " + b + " " + c + " " + texto);
+                x = x + 3;
+
+            }
+        }
+    }
+}
