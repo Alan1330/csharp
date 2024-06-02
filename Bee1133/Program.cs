352278ed08fdd180f8a0b830477e638b1535f309
ALAN TERTULIANO <alan_cfo@outlook.com>
2024-06-02 07:12:24 +0100

    curso

---
+++ Bee1133/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Bee1133 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+
+            int min, max;
+            if (x > y) {
+                max = x;
+                min = y;
+            }
+            else {
+                max = y;
+                min = x;
+            }
+
+            for (int i = min + 1; i < max; i++) {
+                if (i % 5 == 2 || i % 5 == 3) {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
